7b23653d548aa955823c91f6b61082300a83dfa4
jamtechdev <jamtechdev@gmail.com>
2025-02-07 15:38:07 +0530

    adding region API

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const AuthController = require("../controllers/User/AuthController");
 const UserController = require("../controllers/User/UserController");
 const ModelProfileController = require("../controllers/ModalController/ModelProfileController");
-const ContentController = require('../controllers/ContentController/ContentController')
+const ContentController = require('../controllers/ContentController/ContentController');
+const regionController = require("../controllers/RegionController/regionController");
 
 const router = express.Router();
 router.use("/auth", AuthController.getRouter());
 router.use("/profile", UserController.getRouter());
-router.use("/model",ModelProfileController.getRouter())
-router.use("/content", ContentController.getRouter())
+router.use("/model",ModelProfileController.getRouter());
+router.use("/content", ContentController.getRouter());
+router.use("/region", regionController.getRouter());
 
 
 module.exports = router;
