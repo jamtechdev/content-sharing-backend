eb1172e1936e87cbfe8194c80e8984169c8a7922
jamtechdev <jamtechdev@gmail.com>
2025-02-18 18:29:51 +0530

    fixing_bookmarks

--- src/models/Bookmarks.js
+++ src/models/Bookmarks.js
@@ -36,6 +36,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "Bookmarks",
+      tableName:"bookmarks",
       createdAt: "created_at",
       updatedAt: "updated_at",
     }
