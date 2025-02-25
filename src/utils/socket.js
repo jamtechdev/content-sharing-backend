75c79a23a5abc96f1c07b63df57225841243ec2f
jamtechdev <jamtechba@gmail.com>
2025-02-25 17:32:51 +0530

    updated sockethandler for media file

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -41,12 +41,11 @@
 //           const fileExt = path.extname(originalName);
 //           const fileName = `${uuidv4()}${fileExt}`;
 //           const filePath = path.join(__dirname, '../uploads', fileName);
-        
+
 //           fs.writeFileSync(filePath, Buffer.from(buffer)); // Convert buffer to a Node.js Buffer
 //           fileData = { fileName, originalName, filePath };
 //           console.log("File data =>>",fileData);
 //         }
-        
 
 //         await createChat({ message, senderId, receiverId: to });
 //         io.to(users[to]).emit('receiveMessage', { from: senderId, message, file: fileData });
@@ -71,67 +70,84 @@
 
 // module.exports = socketHandler;
 
-
-
-
-
-
 // --------------------- Working Code for Chat Text Only 25/02/2025 -----------------------
 
 // const db = require('../models/index')
 // const Message = db.Message
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const {createChat} = require('../repositories/MessageRepository')
+const { createChat } = require("../repositories/MessageRepository");
 
-
-
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 const users = {};
 
-const socketHandler = (io) =>{
-  io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-  
-    socket.on('authenticate', (token) => {
+const socketHandler = (io) => {
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("authenticate", (token) => {
       try {
         const decoded = jwt.verify(token, JWT_SECRET);
         users[decoded.userId] = socket.id;
         socket.userId = decoded.userId;
         console.log(`User registered: ${decoded.userId}`);
-        console.log('Current users:', users);
+        console.log("Current users:", users);
       } catch (error) {
-        console.log('Authentication error:', error);
+        console.log("Authentication error:", error);
         socket.disconnect();
       }
     });
-  
-    socket.on('sendMessage', async ({ to, message, file, from, timestamp }) => {
-      const senderId = socket.userId;
-      console.log(`Sending message from ${senderId} to ${to}: ${message}`);
-  
-      if (!users[to]) {
-        console.log(`User ${to} is offline.`);
-        return;
-      }
-  
-      try {
-        if(!file){
-          
-          await createChat({ message, senderId, receiverId: to, mediaType: "text" })
+
+    socket.on(
+      "sendMessage",
+      async ({
+        to,
+        message,
+        mediaType,
+        mediaUrl,
+        mediaSize,
+        from,
+        timestamp,
+      }) => {
+        const senderId = socket.userId;
+        console.log(`Sending message from ${senderId} to ${to}: ${message}`);
+
+        if (!users[to]) {
+          console.log(`User ${to} is offline.`);
+          return;
         }
-        // const savedMessage = await Message.create({ message, senderId, receiverId: to });
-        // console.log('Message saved to DB:');
-  
-        io.to(users[to]).emit('receiveMessage', { from: senderId, message, file, to:to, timestamp });
-        console.log(`Message sent to ${to}: ${message}`);
-      } catch (error) {
-        console.error('Error saving message:', error);
+
+        try {
+          if (mediaType === "text") {
+            await createChat({ message, senderId, receiverId: to, mediaType });
+          } else{
+            await createChat({
+              senderId,
+              receiverId: to,
+              mediaUrl,
+              mediaSize,
+              mediaType: mediaType=== "image"? "image": "video",
+            });
+          }
+          // const savedMessage = await Message.create({ message, senderId, receiverId: to });
+          // console.log('Message saved to DB:');
+
+          io.to(users[to]).emit("receiveMessage", {
+            from: senderId,
+            message,
+            file,
+            to: to,
+            timestamp,
+          });
+          console.log(`Message sent to ${to}: ${message}`);
+        } catch (error) {
+          console.error("Error saving message:", error);
+        }
       }
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
+    );
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
       for (let userId in users) {
         if (users[userId] === socket.id) {
           delete users[userId];
@@ -140,18 +156,12 @@ const socketHandler = (io) =>{
         }
       }
     });
-  });  
-} 
+  });
+};
 
-module.exports = socketHandler
-
-
-
-
-
+module.exports = socketHandler;
 
 // // App.js
-
 
 // import React, { useState, useEffect } from 'react';
 // import io from 'socket.io-client';
@@ -242,9 +252,7 @@ module.exports = socketHandler
 
 // export default App;
 
-
 // // index.js
-
 
 // const express = require('express');
 // const http = require('http');
@@ -374,26 +382,8 @@ module.exports = socketHandler
 
 // server.listen(8000, () => console.log('Server running on port 8000'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // App.js
 // // ==============
-
 
 // import React, { useState, useEffect } from 'react';
 // import io from 'socket.io-client';
@@ -483,12 +473,8 @@ module.exports = socketHandler
 
 // export default App;
 
-
-
-
 // // index.js (backend)
 // // ======================
-
 
 // const express = require('express');
 // const http = require('http');
@@ -596,4 +582,3 @@ module.exports = socketHandler
 // });
 
 // server.listen(8000, () => console.log('Server running on port 8000'));
-
