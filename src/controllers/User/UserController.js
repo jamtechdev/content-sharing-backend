98be7735150c5cf3c633657ec464ccefd08d0d5a
jamtechdev <jamtechdev@gmail.com>
2025-02-25 19:46:50 +0530

    fixing user controller

--- src/controllers/User/UserController.js
+++ src/controllers/User/UserController.js
@@ -57,7 +57,6 @@ class UserController {
       });
     } else {
       const newUser = await UserService.getUserById(user?.userId);
-      return newUser;
       return res.status(200).json({
         code: 200,
         data: {
