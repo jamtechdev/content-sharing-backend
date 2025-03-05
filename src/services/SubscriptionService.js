0fd970e86abef0b1e12fe1b2f30c4e98aec8b4cc
jamtechdev <jamtechdev@gmail.com>
2025-03-05 16:31:36 +0530

    fixing strip

--- src/services/SubscriptionService.js
+++ src/services/SubscriptionService.js
@@ -5,8 +5,6 @@ const cron = require("node-cron");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-
-
 class SubscriptionService {
   async createSubscription(priceId, data) {
     const { subscriber_id, model_id, plan_id } = data;
@@ -33,8 +31,8 @@ class SubscriptionService {
           quantity: 1,
         },
       ],
-      success_url: `http://localhost:3000/my/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://localhost:3000/cancel`,
+      success_url: `http://reinarancy.com/my/success/my/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `http://reinarancy.com/my/cancel`,
       customer_email: data.email,
       metadata: {
         subscriber_id: data?.id,
